120d17f1800c54b62b1112e4fa584ca4628fa9c4
baonh1972-byte <baonh1972@gmail.com>
2026-03-16 22:44:53 +0700

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-# main.py
-def greet(name):
-    """Greets the person passed in as a parameter."""
-    print(f"Hello, {name}!")
-
-if __name__ == "__main__":
-    user_name = input("Enter your name: ")
-    greet(user_name)
