de98d2df586b851e3c6ddea767fb46d5b1245e5c
baonh1972-byte <baonh1972@gmail.com>
2026-03-17 10:30:11 +0700

    commit & sync

--- test/test_calculator.py
+++ test/test_calculator.py
@@ -1,10 +1,10 @@
 import unittest
-from caculator import add
+from calculator import add
 
 class TestMyCode(unittest.TestCase):
     def test_add_function(self):
-        # Kiểm tra xem 2 + 3 có đúng bằng 5 không
-        self.assertEqual(add(2, 3), 5)
+        # Kiểm tra xem 2 - 3 có đúng bằng -1 không
+        self.assertEqual(add(2, 3), -1)
 
     def test_add_negative(self):
         # Kiểm tra số âm
